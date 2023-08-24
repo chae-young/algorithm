2124b66745894b58bbd8fab0c399a6cca1b3c604
chae-young <cycy8527@gmail.com>
2023-08-24 10:36:25 +0900

    코테: 중복문자열 제거 풀이

---
+++ backjoon/중복문자열제거.js
@@ -0,0 +1,23 @@
+function getStr(str){
+    let sliceStr = [];
+    let resultStr = 0;
+
+    for(let i = 0; i < str.length; i++){
+        //중복되어 있지 않으면
+        if(sliceStr.indexOf(str[i]) === -1){
+            sliceStr.push(str[i])
+            if(resultStr < sliceStr.length){
+                resultStr - sliceStr.length
+            }
+
+        }else{
+            //이미 저장된 문자열에서 중복된 문자부터 제외하고
+            sliceStr = sliceStr.slice( sliceStr.indexOf(str[i]) + 1 )
+            //현재문자 추가.
+            sliceStr.push(str[i])
+        }
+    }
+
+    return resultStr
+}
+getStr('helloworld')
