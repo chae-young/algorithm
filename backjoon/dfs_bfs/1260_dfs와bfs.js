39ed00eb536ce1aea4dd664f03d46dce1169241f
chae-young <cycy8527@gmail.com>
2023-12-05 14:56:45 +0900

    Update

---
+++ backjoon/dfs_bfs/1260_dfs와bfs.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const input = fs.readFileSync(__dirname + '/1260_dfs와bfs.txt').toString().trim().split('\n');
+// const input = fs.readFileSync(__dirname + '/index1.txt').toString().split(' ')
+// const input = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ');
+// const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
