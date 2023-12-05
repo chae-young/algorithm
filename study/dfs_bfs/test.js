770a52d4cc6ba715f4b48ec6c6367ef2849e3aa9
chae-young <cycy8527@gmail.com>
2023-12-05 14:58:36 +0900

    Update dfs

---
+++ study/dfs_bfs/test.js
@@ -0,0 +1,22 @@
+
+/*
+-1+1+1+1+1
++1-1+1+1+1
+...
+
++4+1-2+1 = 4
++4-1+2-1 = 4
+
+*/
+function solution(numbers, target) {
+    if(visitedNodes)
+    dfsRecursive(node,visitedNodes){
+
+    }
+
+    dfsRecursive()
+}
+
+const visitedNodes = [];
+
+console.log([4,2,1,2],4)
