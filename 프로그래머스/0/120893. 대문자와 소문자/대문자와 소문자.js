1ea20462dd8f55e6c73a0efb9416627bc1f55880
이챙 <cycy8527@gmail.com>
2025-04-22 22:04:35 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.57 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -1,12 +1,4 @@
 function solution(my_string) {
-    let answer = ''
-    for(const str of my_string) {
-        if(str.toUpperCase() === str) {
-            answer += str.toLowerCase();
-        }else {
-            answer += str.toUpperCase();
-        }
-    }
-    
-    return answer
+    var answer = '';
+    return answer;
 }
